56a0eb15d19b8464beac6182d2bb2c53198be09e
seyoung5744 <seyoung574458@gmail.com>
2025-09-24 20:44:42 +0900

    init project

---
+++ src/main/java/com/sparta/forusmarket/ForUsMarketApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.forusmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForUsMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForUsMarketApplication.class, args);
+    }
+
+}
