ffbe1a32194e43d826a203aeb13e0dda16186875
nyong0313 <ghdyd6625@naver.com>
2025-09-26 10:56:55 +0900

    fix(auth): Login dto @Vaid 추가

--- src/main/java/com/sparta/forusmarket/domain/auth/dto/request/LoginRequest.java
+++ src/main/java/com/sparta/forusmarket/domain/auth/dto/request/LoginRequest.java
@@ -1,4 +1,11 @@
 package com.sparta.forusmarket.domain.auth.dto.request;
 
-public record LoginRequest(String email, String password) {
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequest(
+        @NotBlank
+        String email,
+
+        @NotBlank
+        String password) {
 }
