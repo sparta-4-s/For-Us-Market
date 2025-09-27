d8a999b8a501866e04b22462c4ae782b544df6d2
seyoung5744 <seyoung574458@gmail.com>
2025-09-27 14:18:58 +0900

    test: 단일 상품 조회 서비스 테스트

--- src/test/java/com/sparta/forusmarket/domain/product/service/ProductServiceTest.java
+++ src/test/java/com/sparta/forusmarket/domain/product/service/ProductServiceTest.java
@@ -1,16 +1,21 @@
 package com.sparta.forusmarket.domain.product.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 
 import com.sparta.forusmarket.domain.product.dto.request.ProductRegisterRequest;
 import com.sparta.forusmarket.domain.product.dto.response.ProductResponse;
 import com.sparta.forusmarket.domain.product.entity.Product;
+import com.sparta.forusmarket.domain.product.exception.ProductNotFoundException;
 import com.sparta.forusmarket.domain.product.repository.ProductRepository;
 import com.sparta.forusmarket.utils.TestUtils;
 import java.util.Map;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -50,4 +55,44 @@ class ProductServiceTest {
                 .extracting("id", "name")
                 .contains(1L, productName);
     }
+
+    @Nested
+    @DisplayName("단일 상품 조회")
+    class GetProduct {
+        @Test
+        @DisplayName("단일 상품 조회 성공")
+        void getProductById() {
+            // given
+            final String productName = "프라다 리나일론 체인 스트랩 호보백";
+            final Long productId = 1L;
+            Product product = TestUtils.createEntity(Product.class, Map.of(
+                    "id", productId,
+                    "name", productName
+            ));
+
+            given(productRepository.findById(anyLong())).willReturn(Optional.of(product));
+
+            // when
+            ProductResponse productResponse = productService.getProductById(productId);
+
+            // then
+            assertThat(productResponse)
+                    .extracting("id", "name")
+                    .contains(1L, productName);
+        }
+
+        @Test
+        @DisplayName("존재하지 않은 상품 ID 조회 시 에러 응답")
+        void getProductById_WhenProductNotFound_ThrowsException() {
+            // given
+            final Long productId = 1L;
+            given(productRepository.findById(anyLong())).willReturn(Optional.empty());
+
+            // when & then
+            assertThatThrownBy(() -> productService.getProductById(productId))
+                    .isInstanceOf(ProductNotFoundException.class)
+                    .hasMessage("존재하지 않은 상품입니다.");
+        }
+    }
+
 }
