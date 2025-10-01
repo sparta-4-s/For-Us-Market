cf9902d6183c5b7d54bfd8dafc4ff39b4cb222af
eunseo04 <jes040616@naver.com>
2025-10-02 01:04:58 +0900

    feat: 스케줄링 및 검색횟수 업데이트 쿼리

--- src/main/java/com/sparta/forusmarket/domain/hotKeywords/repository/HotKeywordsRepository.java
+++ src/main/java/com/sparta/forusmarket/domain/hotKeywords/repository/HotKeywordsRepository.java
@@ -2,8 +2,18 @@ package com.sparta.forusmarket.domain.hotKeywords.repository;
 
 import com.sparta.forusmarket.domain.hotKeywords.entity.HotKeywords;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface HotKeywordsRepository extends JpaRepository<HotKeywords, Long> {
 
     HotKeywords findByKeyword(String keyword);
+
+    @Modifying // 업데이트 쿼리를 실행하기 위한 JPA어노테이션
+    @Query("Update HotKeywords hk SET hk.searchCount = :newview WHERE hk.keyword = :keyword")
+    //매개변수로 준 keyword에 해당하는 keyword 조회수칼럼을 매개변수로 준 newview로 업데이트한다는 말
+    @Transactional
+    void increaseViewCountByValue(@Param("keyword") String keyword, @Param("newview") Double newview);
 }
--- src/main/java/com/sparta/forusmarket/domain/hotKeywords/service/HotKeywordsService.java
+++ src/main/java/com/sparta/forusmarket/domain/hotKeywords/service/HotKeywordsService.java
@@ -1,8 +1,11 @@
 package com.sparta.forusmarket.domain.hotKeywords.service;
 
 import com.sparta.forusmarket.domain.hotKeywords.dto.HotKeywordsResponse;
+import com.sparta.forusmarket.domain.hotKeywords.repository.HotKeywordsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
@@ -10,11 +13,27 @@ import java.util.Set;
 @Service
 @RequiredArgsConstructor
 public class HotKeywordsService {
+    private final HotKeywordsRepository hotKeywordsRepository;
     private final RedisTemplate<String, Object> redisTemplate;
 
     public HotKeywordsResponse getHotKeywords() {
         String key = "product:ranking";
         Set<Object> top5 = redisTemplate.opsForZSet().reverseRange(key, 0, 4);
         return HotKeywordsResponse.of(top5);
+    }
+
+    @Scheduled(cron = "0 */5 * * * * ") //5분마다 db에 업데이트
+    public void getAllViewCountForRdb() {
+        String key = "product:ranking"; //zset은 Spring에서 TypedTuple로 표현됨.
+        // 전체 멤버 + 점수 (낮은 점수부터 높은 점수까지)
+        Set<ZSetOperations.TypedTuple<Object>> all = redisTemplate.opsForZSet().rangeWithScores(key, 0, -1);
+
+        if (all != null) {
+            for (ZSetOperations.TypedTuple<Object> tuple : all) {
+                String keyword = (String) tuple.getValue(); // 상품명 (member)
+                Double score = tuple.getScore();            // 조회수 (score)
+                hotKeywordsRepository.increaseViewCountByValue(keyword, score);
+            }
+        }
     }
 }
