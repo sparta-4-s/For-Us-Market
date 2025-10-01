69e752200c57c6f0297dde552b3762ff75c804e2
eunseo04 <jes040616@naver.com>
2025-10-02 01:10:27 +0900

    fix: url restMapping 제거

--- src/main/java/com/sparta/forusmarket/domain/product/controller/ProductController.java
+++ src/main/java/com/sparta/forusmarket/domain/product/controller/ProductController.java
@@ -4,7 +4,6 @@ import com.sparta.forusmarket.common.response.ApiPageResponse;
 import com.sparta.forusmarket.common.response.ApiResponse;
 import com.sparta.forusmarket.domain.product.dto.request.ProductEditRequest;
 import com.sparta.forusmarket.domain.product.dto.request.ProductRegisterRequest;
-import com.sparta.forusmarket.domain.product.dto.response.ProductPageResponse;
 import com.sparta.forusmarket.domain.product.dto.response.ProductResponse;
 import com.sparta.forusmarket.domain.product.service.ProductService;
 import com.sparta.forusmarket.domain.product.type.SubCategoryType;
@@ -13,25 +12,26 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-//@RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
 public class ProductController {
 
     private final ProductService productService;
+    private final RedisTemplate<String, Object> redisTemplate;
 
-    @PostMapping
+    @PostMapping("/api/v1/products")
     public ResponseEntity<ApiResponse<ProductResponse>> createProduct(
             @Valid @RequestBody ProductRegisterRequest request) {
         ProductResponse productResponse = productService.createProduct(request);
         return ApiResponse.created(productResponse);
     }
 
-    @GetMapping
+    @GetMapping("/api/v1/products")
     public ResponseEntity<ApiPageResponse<ProductResponse>> getProducts(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size,
@@ -42,13 +42,13 @@ public class ProductController {
         return ApiPageResponse.success(productResponses);
     }
 
-    @GetMapping("/{productId}")
+    @GetMapping("/api/v1/products/{productId}")
     public ResponseEntity<ApiResponse<ProductResponse>> getProduct(@PathVariable Long productId) {
         ProductResponse productResponse = productService.getProductById(productId);
         return ApiResponse.success(productResponse);
     }
 
-    @PutMapping("/{productId}")
+    @PutMapping("/api/v1/products/{productId}")
     public ResponseEntity<ApiResponse<ProductResponse>> editProduct(@Valid @RequestBody ProductEditRequest request,
                                                                     @PathVariable Long productId) {
         ProductResponse productResponse = productService.editProduct(productId, request);
@@ -63,10 +63,12 @@ public class ProductController {
     }
 
     @GetMapping("/api/v2/products/search")
-    public ResponseEntity<ProductPageResponse<ProductResponse>> searchByCaching(@RequestParam(required = false) String name,
-                                                                                @RequestParam(required = false) SubCategoryType category,
-                                                                                @PageableDefault(page = 0, size = 10) Pageable pageable) {
-        return ResponseEntity.ok(productService.searchByCaching(name, category, pageable));
+    public ResponseEntity<ApiPageResponse<ProductResponse>> searchByCaching(@RequestParam(required = false) String name,
+                                                                            @RequestParam(required = false) SubCategoryType category,
+                                                                            @PageableDefault(page = 0, size = 10) Pageable pageable) {
+        String key = "product:ranking";
+        redisTemplate.opsForZSet().incrementScore(key, name, 1); //검색 시 캐시 score 증가
+        return ApiPageResponse.success(productService.searchByCaching(name, category, pageable));
     }
 
     @DeleteMapping("/api/v2/products/search")
