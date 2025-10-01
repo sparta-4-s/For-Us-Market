0b260f8a2f58bff89fe48c26360adb0db07e0646
eunseo04 <jes040616@naver.com>
2025-10-02 01:06:42 +0900

    fix: redis 페이징 에러 수정

--- src/main/java/com/sparta/forusmarket/common/response/ApiPageResponse.java
+++ src/main/java/com/sparta/forusmarket/common/response/ApiPageResponse.java
@@ -1,19 +1,22 @@
 package com.sparta.forusmarket.common.response;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Getter
-public class ApiPageResponse<T> {
+@NoArgsConstructor
+public class ApiPageResponse<T> implements Serializable {
 
-    private final int page;
-    private final int size;
-    private final int totalPages;
-    private final long totalElements;
-    private final List<T> data;
+    private int page;
+    private int size;
+    private int totalPages;
+    private long totalElements;
+    private List<T> data;
 
     private ApiPageResponse(int page, int size, int totalPages, long totalElements, List<T> data) {
         this.page = page;
@@ -24,14 +27,27 @@ public class ApiPageResponse<T> {
     }
 
     /**
-     * 성공적인 요청에 대한 페이징 응답을 반환하는 메서드
-     * 주어진 데이터를 포함하여 HTTP 200 OK 상태 코드와 함께 응답을 반환
+     * 성공적인 요청에 대한 페이징 응답을 반환하는 메서드 주어진 데이터를 포함하여 HTTP 200 OK 상태 코드와 함께 응답을 반환
      *
      * @param pagedData 요청 성공 시 반환할 페이징 데이터
      * @return HTTP 200 OK 응답과 함께 성공 데이터가 포함된 ApiPageResponse
      */
+    public static <T> ResponseEntity<ApiPageResponse<T>> success(RestPage<T> pagedData) {
+        return ResponseEntity.ok(fromPage(pagedData));
+    }
+
     public static <T> ResponseEntity<ApiPageResponse<T>> success(Page<T> pagedData) {
         return ResponseEntity.ok(fromPage(pagedData));
+    }
+
+    private static <T> ApiPageResponse<T> fromPage(RestPage<T> pagedData) {
+        return new ApiPageResponse<>(
+                pagedData.getPageable().getPageNumber(),
+                pagedData.getPageable().getPageSize(),
+                pagedData.getTotalPages(),
+                pagedData.getTotalElements(),
+                pagedData.getContent()
+        );
     }
 
     private static <T> ApiPageResponse<T> fromPage(Page<T> pagedData) {
